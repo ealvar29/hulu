2a4ca5a10ad5c94837d91dac4fb68b9992479403
Eduardo <ealvarez@carminaticonsulting.com>
2021-06-13 23:02:39 -0500

    nav issues now

--- hulu-2/components/Nav.js
+++ hulu-2/components/Nav.js
@@ -1,12 +1,15 @@
 import requests from "../utils/requests";
+import { useRouter } from "next/router";
 
 function Nav() {
+  const router = useRouter();
   return (
     <nav className="relative">
       <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl scrollbar-hide sm:px-20 whitespace-nowrap sm:space-x-20">
-        {Object.entries(requests).map(([key, { title, URL }]) => (
+        {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
+            onClick={() => router.push(`/genre=${key}`)}
             className="transition duration-100 transform cursor-pointer last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
