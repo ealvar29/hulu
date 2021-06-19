03086ef427ead485bb8e415940987b731abfffe1
Eduardo <ealvarez@carminaticonsulting.com>
2021-06-19 13:31:34 -0500

    results component completed

--- hulu-2/components/Thumbnail.js
+++ hulu-2/components/Thumbnail.js
@@ -5,7 +5,7 @@ export default function Thumbnail({ result }) {
   console.log(result);
   const baseUrl = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
+    <div className="p-2 transition duration-200 ease-in transform cursor-pointer group sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         height={1000}
@@ -17,11 +17,15 @@ export default function Thumbnail({ result }) {
       />
       <div className="p-2">
         <p className="max-w-md truncate">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
-        <p className="flex items-center">
+
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.original_name}
+        </h2>
+
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} *`}{" "}
           {result.release_date || result.first_air_date} *{" "}
-          <ThumbUpIcon className="h-1 mx-2" />
+          <ThumbUpIcon className="h-5 mx-2" />
           {result.vote_count}
         </p>
       </div>
